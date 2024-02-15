7fcbfc0ac16aff564c47effc8c23e20b6483058d
myles-hash <mylesbillington@gmail.com>
2024-02-15 15:40:06 +0000

    try

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -2,19 +2,13 @@ import { db } from "@/db";
 import auth from "../app/middleware";
 import { revalidatePath } from "next/cache";
 import { VoteButton } from "./VoteButton";
-import { LoginButton } from "./LoginButton";
-
-
 
 export async function Vote({ postId, votes }) {
   const session = await auth();
-
   async function upvote() {
     "use server";
     const session = await auth();
-
-    
-    console.log("Downvote", postId, "by user", session.user.id);
+    console.log("Upvote", postId, "by user", session.user.id);
     await db.query(
       "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
       [session.user.id, postId, 1, "post"]
@@ -22,13 +16,11 @@ export async function Vote({ postId, votes }) {
 
     revalidatePath("/");
     revalidatePath(`/post/${postId}`);
-    }
-    // }
+  }
 
   async function downvote() {
     "use server";
     const session = await auth();
-
     console.log("Downvote", postId, "by user", session.user.id);
     await db.query(
       "INSERT INTO votes (user_id, post_id, vote, vote_type) VALUES ($1, $2, $3, $4)",
@@ -36,34 +28,28 @@ export async function Vote({ postId, votes }) {
     );
 
     revalidatePath("/");
-    revalidatePath(`/post/${postId}`); 
+    revalidatePath(`/post/${postId}`);
+  }
 
-
-    }
-
-    if (!session) {
-      return (
-        <div className="max-w-screen-lg mx-auto p-4 mt-10">
-          You need to login to upvote/downvote a post <LoginButton />
-        </div>
-      );
-    }
-  
-
-
+  if (!session) {
+    return (
+      <div className="max-w-screen-lg mx-auto p-4 mt-10">
+        You need to login to upvote/downvote a post <LoginButton />
+      </div>
+    );
+  }
 
   return (
     <>
       {votes} votes
       <div className="flex space-x-3">
-        <form action={upvote} disabled={votes.vote === 1}>
+        <form action={upvote}>
           <VoteButton label="Upvote" />
         </form>
-        <form action={downvote} disabled={votes.vote === 0}>
+        <form action={downvote}>
           <VoteButton label="Downvote" />
         </form>
       </div>
     </>
   );
 }
-
